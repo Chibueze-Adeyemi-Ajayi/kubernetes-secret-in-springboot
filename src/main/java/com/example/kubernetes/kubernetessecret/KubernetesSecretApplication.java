c1006d3dc5db6675f942601379b86a4c8683835d
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-11-12 00:29:04 +0000

    Kubernetes secret

--- src/main/java/com/example/kubernetes/kubernetessecret/KubernetesSecretApplication.java
+++ src/main/java/com/example/kubernetes/kubernetessecret/KubernetesSecretApplication.java
@@ -1,13 +1,27 @@
 package com.example.kubernetes.kubernetessecret;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("api/kubernetes/secret")
 public class KubernetesSecretApplication {
+
+	@Value("${my-secret.data}")
+	String secret_data;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesSecretApplication.class, args);
+	}
+
+	@GetMapping("")
+	public String secret () {
+		return "Kubernetes secret data is: " + secret_data;
 	}
 
 }
